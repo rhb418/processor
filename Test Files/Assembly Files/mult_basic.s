92848098c2f59f74d93f300541270f3de0b732c4
rhb418 <70110540+rhb418@users.noreply.github.com>
2021-03-29 03:15:33 -0400

    Add files via upload

---
+++ Test Files/Assembly Files/mult_basic.s
@@ -0,0 +1,17 @@
+nop             
+nop            
+nop
+nop           
+nop 		
+addi $3, $0, 1	
+addi $4, $0, 35	
+addi $1, $0, 3	
+addi $2, $0, 21	
+sub $3, $0, $3	
+sub $4, $0, $4	
+nop
+nop
+mul $19, $2, $1
+div $20, $2, $1
+mul $21, $3, $1	
+div $22, $4, $1	
