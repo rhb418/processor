e401a718f994aa4854101042e78b2c2f08e66b49
Robert Barnette <rhb19@duke.edu>
2021-03-29 18:09:18 -0400

    test regcontrol update

--- regFileAndSXControl.v
+++ regFileAndSXControl.v
@@ -5,7 +5,7 @@ input multOrDivReady;
 output [4:0] readA, readB, writeD; 
 output wE; 
 
-wire [4:0] MWOP, FDOP, PWOP, FDRD, FDRS, FDRT, readBAluOrLoadStore, aluOpWriteD;
+wire [4:0] MWOP, FDOP, PWOP, FDRD, FDRS, FDRT, readBAluOrLoadStore, aluOpWriteD, jrWD, readBBranch, readABranch;
 assign MWOP = MWIR[31:27];
 assign FDOP = FDIR[31:27];
 assign PWOP = PWIR[31:27];
@@ -14,19 +14,23 @@ assign FDRD = FDIR[26:22];
 assign FDRS = FDIR[21:17];
 assign FDRT = FDIR[16:12];
 
-wire isBranch, isLoadOrStore, isAluOP, isBranchOrJr; 
+wire isBranch, isLoadOrStore, isAluOP, isBranchOrJr, isEx; 
 
 assign isBranchOrJr = (FDOP == 2) || (FDOP == 6) || (FDOP ==4); 
 assign isLoadOrStore = (FDOP == 7) || (FDOP ==8); 
 assign isAluOP = (FDOP == 0); 
+assign isEx = (FDOP == 21) || (FDOP ==22);
 
 
 
-assign readA = isBranchOrJr ? FDRD : FDRS;
+assign readABranch = isBranchOrJr ? FDRD : FDRS;
+assign readA = (isEx) ? 0 : readABranch;
 assign readBAluOrLoadStore = isLoadOrStore ? FDRD : FDRT;
-assign readB = isBranchOrJr ? FDRS : readBAluOrLoadStore; 
+assign readBBranch = isBranchOrJr ? FDRS : readBAluOrLoadStore; 
+assign readB = (isEx) ? 30 : readBBranch; 
 
 assign wE = (MWOP == 0)  || ((PWOP == 0) && multOrDivReady) || (MWOP == 5) || (MWOP == 8) || (MWOP ==3); 
 assign aluOpWriteD = multOrDivReady ? PWIR[26:22] : MWIR[26:22];
-assign writeD = (MWOP ==3) ? 31 : aluOpWriteD; 
+assign jrWD = (MWOP ==3) ? 31 : aluOpWriteD; 
+assign writeD = (MWOP == 21) ? 30 : jrWD; 
 endmodule
